b563d4ca44d33c32529aa9afef3008ac88ab59ec
Sam Kessler <sholomkessler1@gmail.com>
2021-01-12 14:15:28 -0500

    Create TableCell.jsx

---
+++ assignment8/src/TableCell.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TableCell extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { Color: props.color };
+	}
+	render() {
+		return <td style={{ backgroundColor: this.state.color }}></td>;
+	}
+}
