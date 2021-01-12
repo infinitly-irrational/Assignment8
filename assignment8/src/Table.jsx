42ec58d9063301a9a70039d4a8ada93ef3f83591
Mohammad Hossain <moh.hossain98@gmail.com>
2021-01-12 17:18:39 -0500

    add/remove column and rows

--- assignment8/src/Table.jsx
+++ assignment8/src/Table.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import './App.css';
 import TableRow from "./TableRow"
 
 class Table extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            rows: ['row']
+            rows: [1],
+            column: [1]
         }
         this.handleAddButtonClick = this.handleAddButtonClick.bind(this)
         this.handleDeleteButtonClick = this.handleDeleteButtonClick.bind(this)
+        this.addColumnButton = this.addColumnButton.bind(this)
+        this.deleteColumnButton = this.deleteColumnButton.bind(this)
     }
 
 
     handleAddButtonClick(){
         var rows = this.state.rows
-        rows.push('new row')
+        rows.push(rows.length + 1)
         this.setState({rows: rows})
-        
         console.log(rows)
         console.log('add button clicked')
 
@@ -28,7 +31,27 @@ class Table extends React.Component{
         this.setState({rows: rows})
         console.log('delete button clicked')
         console.log(rows)
-        console.log('delete button clicked')
+        
+    }
+
+    addColumnButton(){
+        var column = this.state.column
+        column.push(column.length+1)
+        this.setState({column: column})
+        console.log("add column clicked")
+        console.log(column)
+        return <TableRow columnAmount = {column}></TableRow>
+        
+
+    }
+
+    deleteColumnButton(){
+        var column = this.state.column
+        column.pop()
+        this.setState({column: column})
+        console.log("delete column clicked")
+        console.log(column)
+        return <TableRow columnAmount = {column}></TableRow>
     }
 
     render(){
@@ -36,13 +59,14 @@ class Table extends React.Component{
             <div>
                 <button onClick={this.handleAddButtonClick}>Add Row</button>
                 <button onClick={this.handleDeleteButtonClick}>Delete Row</button>
+                <button onClick={this.addColumnButton}>Add Column</button>
+                <button onClick={this.deleteColumnButton}>Delete Column</button>
                 <table className="table">
                     <tbody>
                     {this.state.rows.map((r, index) => (
-                      <TableRow key={index}></TableRow>
+                      <TableRow key={index} columnAmount = {this.state.column}></TableRow>
                     ))}
                     </tbody>
-                    
                 </table>
             </div>
         )
--- assignment8/src/TableRow.jsx
+++ assignment8/src/TableRow.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react'
 import TableCell from "./TableCell"
 
@@ -5,10 +6,18 @@ class TableRow extends React.Component{
     constructor(props){
         super(props)
     }
+    componentDidMount(){
+        console.log(this.props.columnAmount)
+        let arr = Array.from(this.props.columnAmount)
+        console.log(arr)
+    }
     render(){
         return(
                 <tr className="tableRows">
-                    <TableCell></TableCell>
+                   {Array.from(this.props.columnAmount).map((r, index) => (
+                      <TableCell>R</TableCell>
+                    ))} 
+                    
                 </tr>
         )
     }
