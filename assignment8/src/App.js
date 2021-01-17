c680fe6ada0ae9284f97237c100f99649ab5d04f
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2021-01-17 17:55:13 -0500

    Attempted to color table

--- assignment8/src/App.js
+++ assignment8/src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import Table from './Table';
+import "./App.css";
+import Table from "./Table";
+import "./TableColor.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Table dimensions={{ x: 2, y: 3 }} />
-		</div>
-	);
+  function myStyle(event) {
+    const val = event.target.value;
+
+    return { backgroundColor: val };
+  }
+
+  return (
+    <div className="App">
+      <div id="colorPicker">
+        <form>
+          <label for="color">Select a color:</label>
+          <select onChange={myStyle}>
+            <option id="red" value="red">
+              Red
+            </option>
+            <option id="green" value="green">
+              Green
+            </option>
+            <option id="blue" value="blue">
+              Blue
+            </option>
+            <option id="black" value="black">
+              Black
+            </option>
+          </select>
+        </form>
+      </div>
+      <Table id="table" style={{ myStyle }} dimensions={{ x: 2, y: 3 }} />
+    </div>
+  );
 }
 
 export default App;
--- assignment8/src/TableCell.jsx
+++ assignment8/src/TableCell.jsx
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import { defaultColor } from './Table';
+import React, { Component } from "react";
+import { defaultColor } from "./Table";
+import "./TableColor.css";
 
 class TableCell extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { Color: props.color };
-		this.updateCellColorPipe = this.updateCellColorPipe.bind(this);
-	}
-	updateCellColorPipe() {
-		this.props.updateColorOfCell(this.props.index, this.getColor());
-	}
-	getColor() {
-		console.log('fetching color');
-		return defaultColor;
-	}
-	render() {
-		return <td style={{ backgroundColor: this.state.color }} onClick={this.updateCellColorPipe}></td>;
-	}
+  constructor(props) {
+    super(props);
+    this.state = { Color: props.color };
+    this.updateCellColorPipe = this.updateCellColorPipe.bind(this);
+  }
+  updateCellColorPipe() {
+    this.props.updateColorOfCell(this.props.index, this.getColor());
+  }
+  getColor() {
+    console.log("fetching color");
+    return defaultColor;
+  }
+  render() {
+    return (
+      <td
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.updateCellColorPipe}
+      ></td>
+    );
+  }
 }
 
 export default TableCell;
